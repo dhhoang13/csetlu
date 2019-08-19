33730dc65dee2722048f8056b11511049916f053
dhhoang13 <daohuyhoang1995@gmail.com>
2019-08-19 22:24:10 +0700

    website student manager

---
+++ diemdanh.php
@@ -0,0 +1,11 @@
+<?php
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		diem danh
+	</div><!-- #primary -->
+
+<?php
+scholarship_get_sidebar();
+get_footer();
---
+++ wp-content/plugins/custom/custom.php
@@ -0,0 +1,10 @@
+<?
+/**
+* Plugin Name: Custom Plugin
+* Plugin URI: https://www.yourwebsiteurl.com/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: lucas
+* Author URI: http://yourwebsiteurl.com/
+**/
+?>
